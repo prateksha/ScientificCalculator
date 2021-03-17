248a5679c858baae1c362fa0e53893de65d0ce9b
prateksha <pratekshau@gmail.com>
2021-03-17 10:19:54 +0530

    Added basic tests

--- src/test/java/ScientificCalculatorTest.java
+++ src/test/java/ScientificCalculatorTest.java
@@ -9,10 +9,43 @@ public class ScientificCalculatorTest {
     }
 
     @Test
-    public void testSqRoot() {
+    public void testSqRootInt() {
         double a = 25;
         double expectedRes = 5;
         double res = calculator.sqroot(a);
+        Assert.assertEquals(expectedRes, res, 0.001);
+    }
+
+    @Test
+    public void testSqRootDouble() {
+        double a = 20;
+        double expectedRes = 4.472;
+        double res = calculator.sqroot(a);
+        Assert.assertEquals(expectedRes, res, 0.001);
+    }
+
+    @Test
+    public void testFactorial() {
+        int a = 5;
+        int expectedRes = 120;
+        int res = calculator.factorial(a);
+        Assert.assertEquals(expectedRes, res);
+    }
+
+    @Test
+    public void testNaturalLog() {
+        double a = 2.718;
+        double expectedRes = 1;
+        double res = calculator.natural_log(a);
+        Assert.assertEquals(expectedRes, res, 0.001);
+    }
+
+    @Test
+    public void testPow() {
+        double a = 2;
+        double b = 3;
+        double expectedRes = 8;
+        double res = calculator.pow(a,b);
         Assert.assertEquals(expectedRes, res, 0.001);
     }
 }
